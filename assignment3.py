bcee66c389e9c44903ae02eb2a5b9f6ba72bddaa
gaurav chakarwarti <136673555+Gauravchakarwarti@users.noreply.github.com>
2023-06-26 17:12:18 +0530

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,20 @@
+#Write a Python function to sum all the numbers in a list.
+
+
+#Sample List : (8, 2, 3, 0, 7)
+
+#Expected Output : 20
+
+
+#Explanation:
+
+
+#Summation should like 8+2+3+0+7 = 20
+
+
+
+#solution
+
 def sum_numbers(numbers):
     return sum(numbers)
 sample_list = [8,2,3,0,7]
